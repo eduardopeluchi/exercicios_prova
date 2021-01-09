3a2d5ff8f1a52942d58ac51373e81d50ced9b72d
eduardopeluchi <eduardopeluchi@outlook.com>
2021-01-09 18:15:49 -0300

    Resolucao Exercicio 1

---
+++ Exercicio1.py
@@ -0,0 +1,21 @@
+def exercicio(parametro):
+    contador = 0
+    variavel_teste = 0
+    resultado = 0
+    lista_ordenada = sorted(parametro)
+
+    while contador < len(lista_ordenada):
+        testar_multiplos_tres = int(lista_ordenada[contador] % 3)
+        if testar_multiplos_tres == 0:
+            if lista_ordenada[contador] > variavel_teste:
+                variavel_teste = lista_ordenada[contador]
+
+        resultado = variavel_teste
+        contador += 1
+
+    print(resultado)
+
+
+lista_inteiros = [-6, -91, 1011, -100, 84, -22, 0, 1, 473]
+
+exercicio(lista_inteiros)
